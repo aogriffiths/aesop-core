3b384920aa65bf50785cc45fb23a70a1610b4dc1
Daisy Griffiths <Daisy@mc16917.home>
2015-10-25 12:38:59 +0000

    beautify

--- public/includes/components/component-parallax.php
+++ public/includes/components/component-parallax.php
@@ -87,7 +87,7 @@ if ( ! function_exists( 'aesop_parallax_shortcode' ) ) {
 
 						if ( 'on' == $atts['parallaxbg'] ) { ?>
 
-				   			img.parallax({speed: 0.1});
+				   			img.parallax({speed: 0.8});
 
 		        		<?php }//end if
 
